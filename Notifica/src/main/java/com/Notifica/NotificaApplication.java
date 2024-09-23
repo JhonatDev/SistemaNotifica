47568894133ad68fa480c85f7c212f6ab6d923da
FBurigo <felipeb@innet.com.br>
2024-09-23 19:27:22 -0300

    teste second

---
+++ Notifica/src/main/java/com/Notifica/NotificaApplication.java
@@ -0,0 +1,13 @@
+package com.Notifica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificaApplication.class, args);
+	}
+
+}
---
+++ Notifica/src/test/java/com/Notifica/NotificaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Notifica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
