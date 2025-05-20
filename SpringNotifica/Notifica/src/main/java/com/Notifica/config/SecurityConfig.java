8f653df57c7d66e1815036f2c4803533dbcc125f
FBurigo <felipeb@innet.com.br>
2025-05-20 20:17:36 -0300

    Coisinhas 2.0 *-*

--- SpringNotifica/Notifica/src/main/java/com/Notifica/config/SecurityConfig.java
+++ SpringNotifica/Notifica/src/main/java/com/Notifica/config/SecurityConfig.java
@@ -93,10 +93,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList(
-            "http://localhost:4200",
-            "http://192.168.3.101"
-        ));
+config.setAllowedOrigins(Arrays.asList(
+        "http://localhost:4200",
+        "http://192.168.3.101",
+        "https://192.168.3.101"
+));
+
         config.setAllowedMethods(Arrays.asList(
             "GET", "POST", "PUT", "DELETE", "OPTIONS"
         ));
